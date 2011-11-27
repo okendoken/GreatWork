20e9396fe79b58845fd6e6e9dbde06bc76ba4559
Philip Daineko <daineko.f@gmail.com>
2011-11-27 20:05:14 +0300

    changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [:index, :show]
+  #http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [:index, :show]
   # GET /posts
   # GET /posts.xml
   def index
