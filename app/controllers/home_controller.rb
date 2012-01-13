bee5c3f1328b389566a467ce65428ef8416c1d6a
Philip Daineko <daineko.f@gmail.com>
2012-01-14 01:44:49 +0300

    layout changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
   def index
+    redirect_to current_user if user_signed_in?
   end
 
 end
--- db/questions.sql
+++ db/questions.sql
@@ -1,4 +1,4 @@
-INSERT INTO `questions` (`id`, `questionText`, `created_at`, `updated_at`) VALUES
+INSERT INTO questions ('id', 'questionText', 'created_at', 'updated_at') VALUES
 (1, 'What are your talents?', '2012-01-12 21:14:50', '2012-01-12 21:15:31'),
 (2, 'What excites you?', '2012-01-12 21:15:56', '2012-01-12 21:15:56'),
 (3, 'What do you read about?', '2012-01-12 21:16:38', '2012-01-12 21:16:38');
